47adedfcc4d38289a9445e40722bfa17c1527ef3
maciejtreder <contact@maciejtreder.com>
2017-06-10 22:22:59 +0200

    adding uglifyjs

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,4 +1,35 @@
-/**
- * This is a prod config to be merged with the Client config
- */
-module.exports = {};
+const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+
+
+module.exports = {
+    plugins: [
+        new OptimizeJsPlugin({
+            sourceMap: false
+        }),
+        new UglifyJsPlugin({
+            beautify: false, //prod
+            sourceMap: true,
+            output: {
+                comments: false
+            }, //prod
+            mangle: {
+                screw_ie8: true
+            }, //prod
+            compress: {
+                screw_ie8: true,
+                warnings: false,
+                conditionals: true,
+                unused: true,
+                comparisons: true,
+                sequences: true,
+                dead_code: true,
+                evaluate: true,
+                if_return: true,
+                join_vars: true,
+                negate_iife: false // we need this for lazy v8
+            },
+        }),
+    ]
+};
