669052bfdaa679705d9fca6c8c63d6369c3e89df
maciejtreder <contact@maciejtreder.com>
2017-06-11 15:14:29 +0200

    update main.server

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,6 +2,11 @@ import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
 import 'rxjs/Rx';
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import * as compression from 'compression';
+import * as awsServerlessExpressMiddleware from 'aws-serverless-express/middleware';
+
 import { platformServer, renderModuleFactory } from '@angular/platform-server';
 import { ServerAppModule } from './app/server-app.module';
 import { ngExpressEngine } from '@nguniversal/express-engine';
@@ -12,6 +17,13 @@ enableProdMode();
 const app = express();
 const port = 8000;
 const baseUrl = `http://localhost:${port}`;
+
+
+app.use(compression());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(awsServerlessExpressMiddleware.eventContext());
 
 app.engine('html', ngExpressEngine({
   bootstrap: ServerAppModule
