91bd2741b30857c671d911d49b06cb32fbbbe46c
Maciej Treder <contact@maciejtreder.com>
2017-07-19 19:52:36 +0200

    ios render - #12 fix

--- src/app/httpProxy/http-proxy-demo.component.ts
+++ src/app/httpProxy/http-proxy-demo.component.ts
@@ -14,18 +14,10 @@ export class HttpProxyDemoComponent {
     public valueToSend: string;
     public response: Observable<any>;
 
-    public showHideText: string = "Show flow";
-    public showFlow: boolean = false;
-
     constructor(private http: Http) {}
 
     public sendPost():void {
         this.response = this.http.post("testPost", {exampleKey: this.valueToSend}).map(res => res.json());
-    }
-
-    public showHideFlow(): void {
-        this.showFlow = this.showFlow?false:true;
-        this.showHideText = this.showFlow?'Hide flow':'Show flow';
     }
 }
 
