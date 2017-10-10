3bfbf11f76f45d0d0e2acaa5f8a9d5b33baf46bd
Maciej Treder <contact@maciejtreder.com>
2017-10-11 00:10:42 +0200

    snackbar refactoring

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,47 @@
-xdescribe('App component', () => {
-    // beforeEach(() => {
-    //
-    // });
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { MdSnackBarModule, MdToolbarModule } from '@angular/material';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ConnectivityService } from 'ng-http-sw-proxy';
+import * as sinon from 'sinon';
+import { DeviceService } from './services/device.service';
+import { WindowRef } from './windowRef';
+import { ServiceWorkerService } from './services/service-worker.service';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
-    it('Should display properly', () => {
-        expect(true).toBeTruthy();
+let fixture: ComponentFixture<AppComponent>;
+
+const connectivityServiceStub = sinon.createStubInstance(ConnectivityService);
+const serviceWorkerServiceStub = sinon.createStubInstance(ServiceWorkerService);
+const isUpdateAvailable: BehaviorSubject<boolean> = new BehaviorSubject(false);
+serviceWorkerServiceStub.update.returns(isUpdateAvailable);
+
+xdescribe('App component', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [AppComponent],
+            imports: [RouterTestingModule, MdSnackBarModule],
+            providers: [
+                {provide: ConnectivityService, useValue: connectivityServiceStub},
+                DeviceService,
+                WindowRef,
+                {provide: ServiceWorkerService, useValue: serviceWorkerServiceStub},
+            ],
+            schemas: [ NO_ERRORS_SCHEMA ]
+        });
+
+        isUpdateAvailable.next(false);
+
+        fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
     });
+
+    // it('Should display information about update', () => {
+    //     isUpdateAvailable.next(true);
+    //     expect(true).toBeTruthy();
+    // });
+    // it('Should display notifications', async(() => {
+    //     fixture.componentInstance
+    // }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,11 @@ import { Observable } from 'rxjs';
 import { ConnectivityService } from 'ng-http-sw-proxy';
 
 import { DeviceService } from './services/device.service';
-import { SnackBarService } from './services/snack-bar.service';
 import { ServiceWorkerService } from './services/service-worker.service';
+import { MdSnackBar, MdSnackBarConfig } from '@angular/material';
+import { Queue } from 'typescript-collections';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   moduleId: module.id,
@@ -18,13 +21,18 @@ export class AppComponent implements OnInit, AfterViewInit {
     public isDesktop: boolean = this.deviceService.isDesktop();
     public navIsFixed: boolean = false;
 
+    private snackBarNotificationsQueue: Queue<SnackBarNotification> = new Queue<SnackBarNotification>();
+    private snackBarNotificationsForceQueue: Queue<SnackBarNotification> = new Queue<SnackBarNotification>();
+    private actuallyDisplayedNotification: SnackBarNotification = null;
+    private snackbarOpened: boolean = false;
+
     constructor(
         @Inject(PLATFORM_ID) private platformId: any,
-        private snackBarService: SnackBarService,
         private conn: ConnectivityService,
         private deviceService: DeviceService,
         private sws: ServiceWorkerService,
-        private elRef: ElementRef
+        private elRef: ElementRef,
+        private snackBar: MdSnackBar
     ) {}
 
     public ngAfterViewInit(): void {
@@ -41,7 +49,10 @@ export class AppComponent implements OnInit, AfterViewInit {
         if (!isPlatformBrowser(this.platformId)) {
             return;
         }
-        this.sws.update().subscribe((response) => console.log('update', response));
+
+        this.sws.update().filter((response) => response).subscribe(() => {
+            this.addToNotificationQueue({message: 'New version of app is available!', action: 'Launch', force: true} as SnackBarNotification);
+        });
 
         let isOnline: boolean = true;
         this.conn.hasNetworkConnection()
@@ -50,18 +61,71 @@ export class AppComponent implements OnInit, AfterViewInit {
             .subscribe((status: boolean) => {
                 isOnline = status;
                 if (status === false) {
-                    this.snackBarService.showMessage(
-                        'You are offline. All changes will be synced when you will go online again.',
-                        'Close'
-                    );
+                    this.addToNotificationQueue({message: 'You are offline. All changes will be synced when you will go online again.', action: 'Close'} as SnackBarNotification);
                 } else {
-                    this.snackBarService.showMessage('You are online. All data is synced.', 'Ok', 3000);
+                    this.addToNotificationQueue({message: 'You are online. All data is synced.', action: 'Ok', duration: 3, force: true} as SnackBarNotification);
                 }
             });
+    }
+
+    private addToNotificationQueue(notification: SnackBarNotification): void {
+        if (notification.force) {
+            this.snackBarNotificationsForceQueue.enqueue(notification);
+        } else {
+            this.snackBarNotificationsQueue.enqueue(notification);
+        }
+        this.displayNotification();
+    }
+
+    private loadNotificationToDisplay(): void {
+        if (this.snackBarNotificationsForceQueue.size() > 0) {
+            this.actuallyDisplayedNotification = this.snackBarNotificationsForceQueue.dequeue();
+        } else if (this.snackBarNotificationsQueue.size() > 0) {
+            this.actuallyDisplayedNotification = this.snackBarNotificationsQueue.dequeue();
+        } else {
+            this.actuallyDisplayedNotification = null;
+            return;
+        }
+    }
+
+    private displayNotification(): void {
+        if (this.snackbarOpened) {
+            if (!this.actuallyDisplayedNotification.force && this.snackBarNotificationsForceQueue.size() > 0) {
+                this.snackBar.dismiss();
+            }
+            return;
+        }
+        this.loadNotificationToDisplay();
+        if (this.actuallyDisplayedNotification == null) {
+            return;
+        }
+
+        this.snackbarOpened = true;
+
+        const config: MdSnackBarConfig = new MdSnackBarConfig();
+        config.duration = 1000 * this.actuallyDisplayedNotification.duration;
+        config.extraClasses = ['service_worker_snack'];
+        const callback = this.actuallyDisplayedNotification.callback;
+
+        this.snackBar.open(this.actuallyDisplayedNotification.message, this.actuallyDisplayedNotification.action, config).afterDismissed().subscribe(() => {
+            if (callback) {
+                callback();
+            }
+            this.snackbarOpened = false;
+            this.displayNotification();
+        });
     }
 
     private onScroll(): void {
         const rect = this.elRef.nativeElement.querySelector('#content menu').getBoundingClientRect();
         this.navIsFixed = rect.top <  64;
     }
+}
+
+interface SnackBarNotification {
+    message: string;
+    action: string;
+    duration: number;
+    callback: () => void;
+    force: boolean;
 }
