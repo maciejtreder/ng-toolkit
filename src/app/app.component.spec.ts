e28af15bf6015e13a3bc6ac18273d8cc618736a9
Maciej Treder <contact@maciejtreder.com>
2017-10-12 16:20:06 +0200

    application spec + refactoring

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, discardPeriodicTasks, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { MdSnackBarModule, MdToolbarModule } from '@angular/material';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ConnectivityService } from 'ng-http-sw-proxy';
@@ -10,29 +9,47 @@ import { WindowRef } from './windowRef';
 import { ServiceWorkerService } from './services/service-worker.service';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Subject } from 'rxjs/Subject';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SnackBarService } from './services/snack-bar.service';
+import { By } from '@angular/platform-browser';
+import { Observable } from 'rxjs/Observable';
 
 let fixture: ComponentFixture<AppComponent>;
 
-const connectivityServiceStub = sinon.createStubInstance(ConnectivityService);
-const hasConnection: Subject<boolean> = new BehaviorSubject(true);
-connectivityServiceStub.hasNetworkConnection.returns(hasConnection);
+let connectivityServiceStub;
+let hasConnection: Subject<boolean>;
 
-const serviceWorkerServiceStub = sinon.createStubInstance(ServiceWorkerService);
-const isUpdated: Subject<boolean> = new BehaviorSubject(false);
-serviceWorkerServiceStub.update.returns(isUpdated);
+let windowStub;
+let snackBarServiceStub;
 
-xdescribe('App component', () => {
+let serviceWorkerServiceStub;
+let isUpdated: Subject<boolean>;
+
+describe('App component', () => {
     beforeEach(() => {
+        Observable.prototype.debounceTime = function() { return this; }; // workaround for https://github.com/angular/angular/issues/10127
+
+        connectivityServiceStub = sinon.createStubInstance(ConnectivityService);
+        hasConnection = new BehaviorSubject(true);
+        connectivityServiceStub.hasNetworkConnection.returns(hasConnection);
+
+        serviceWorkerServiceStub = sinon.createStubInstance(ServiceWorkerService);
+        isUpdated = new BehaviorSubject(false);
+        serviceWorkerServiceStub.update.returns(isUpdated);
+
+        windowStub = sinon.createStubInstance(WindowRef);
+        windowStub._window = {navigator: {userAgent: 'test'}};
+
+        snackBarServiceStub = sinon.createStubInstance(SnackBarService);
+
         TestBed.configureTestingModule({
             declarations: [AppComponent],
-            imports: [RouterTestingModule, MdSnackBarModule],
+            imports: [RouterTestingModule],
             providers: [
                 DeviceService,
-                WindowRef,
+                {provide: WindowRef, useValue: windowStub},
                 {provide: ConnectivityService, useValue: connectivityServiceStub},
                 {provide: ServiceWorkerService, useValue: serviceWorkerServiceStub},
-                BrowserAnimationsModule
+                {provide: SnackBarService, useValue: snackBarServiceStub}
             ],
             schemas: [ NO_ERRORS_SCHEMA ]
         });
@@ -41,13 +58,34 @@ xdescribe('App component', () => {
         fixture.detectChanges();
     });
 
-    it('Should display information about update', () => {
-        isUpdated.next(true);
-        console.log(fixture.nativeElement);
-        expect(true).toBeTruthy();
+    it('Should contain header, menu and footer', () => {
+        expect(fixture.debugElement.query(By.css('menu'))).toBeDefined();
+        expect(fixture.debugElement.query(By.css('h1'))).toBeDefined();
+        expect(fixture.debugElement.query(By.css('footer'))).toBeDefined();
     });
 
-    // it('Should display notifications', async(() => {
-    //     fixture.componentInstance
-    // }));
+    it('Should check for updates immediatelly after initializaiton', () => {
+        expect(serviceWorkerServiceStub.update.called).toBeTruthy('Did not check for update');
+    });
+
+    it('Should display notification when service worker update is available', () => {
+        isUpdated.next(true);
+        expect(snackBarServiceStub.displayNotification.called).toBeTruthy('Snack bar was not displayed');
+        expect(snackBarServiceStub.displayNotification.calledOnce).toBeTruthy('Snack bar was displayed more then once');
+    });
+
+    it('Should display notification in case of that user is offline', async(() => {
+        hasConnection.next(false);
+        expect(snackBarServiceStub.displayNotification.called).toBeTruthy('Notification was not displayed');
+        expect(snackBarServiceStub.displayNotification.calledOnce).toBeTruthy('Snack bar was displayed more then once');
+    }));
+
+    it('Should display new notification when user is back online', async(() => {
+        hasConnection.next(false);
+        expect(snackBarServiceStub.displayNotification.called).toBeTruthy('Notification was not displayed');
+
+        hasConnection.next(true);
+        expect(snackBarServiceStub.displayNotification.calledTwice).toBeTruthy('Snack bar was not displayed twice');
+        expect(snackBarServiceStub.displayNotification.getCall(1).args[0].force).toBeTruthy('Message about online status should be forced!');
+    }));
 });
