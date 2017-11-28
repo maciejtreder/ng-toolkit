625fbd7bfca089fecadb12d4b024dacd4348394b
Maciej Treder <contact@maciejtreder.com>
2017-11-28 23:27:41 +0100

    SEO tags

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Inject, NgModule } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { ActivatedRoute, NavigationEnd, Router, RouterModule } from '@angular/router';
 import {
     MatButtonModule, MatMenuModule, MatSnackBarModule,
     MatToolbarModule
@@ -15,6 +15,13 @@ import { MenuComponent } from './menu.component';
 import { HomeComponent } from './home/home.component';
 import { NotificationService } from './services/notification.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Meta } from '@angular/platform-browser';
+
+const routes: any[] = [
+    { path: '', component: HomeComponent, data: {title: 'Home', description: 'Home.'}},
+    { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule', data: {title: 'Lazy module', description: 'Lazy module example.'}},
+    { path: 'external', loadChildren: '@angular-universal-serverless/external-module/release#ExternalModule', data: {title: 'External module', description: 'External module example.'}}
+];
 
 @NgModule({
   imports: [
@@ -25,11 +32,7 @@ import { HttpClientModule } from '@angular/common/http';
       ServiceWorkerModule,
       HttpClientModule,
     CommonModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent},
-      { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
-      { path: 'external', loadChildren: '@angular-universal-serverless/external-module/release#ExternalModule'}
-    ]),
+      RouterModule.forRoot(routes),
     ServiceWorkerModule
   ],
   declarations: [ AppComponent, HomeComponent, MenuComponent ],
@@ -41,4 +44,13 @@ import { HttpClientModule } from '@angular/common/http';
   ]
 })
 export class AppModule {
+    private title: string = this.doc.querySelector('title').text;
+    private description: string = this.metaService.getTag('name=description').content;
+
+    constructor(private metaService: Meta, private route: ActivatedRoute, private router: Router, @Inject(DOCUMENT) private doc) {
+        this.router.events.filter((e) => e instanceof NavigationEnd).forEach((e) => {
+            this.metaService.updateTag({content: this.description + ' ' + route.root.firstChild.snapshot.data['description']}, 'name=description');
+            this.doc.querySelector('title').childNodes[0].nodeValue = this.title + ' | ' + route.root.firstChild.snapshot.data['title'];
+        });
+    }
 }
