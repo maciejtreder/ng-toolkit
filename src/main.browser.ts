852c8c62cc7f586a08dc2aaad0ba12d883cdfa22
Maciej Treder <contact@maciejtreder.com>
2018-03-09 09:25:58 +0100

    removing duplicated code

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -7,63 +7,22 @@ import 'rxjs/add/operator/filter';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/fromPromise';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { ApplicationRef, enableProdMode } from '@angular/core';
-import { enableDebugTools } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
 import { bootloader } from '@angularclass/hmr';
 import { BrowserAppModule } from './app/browser-app.module';
-
-declare var FIREBUG: boolean;
+import { decorateModuleRef, googleAnalytics, fireBug } from './main.browser.shared';
 
 if (process.env.NODE_ENV === 'production') {
     enableProdMode();
-
-    const script = document.createElement('script');
-    const scriptGA = document.createElement('script');
-    scriptGA.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=UA-109145893-2');
-    script.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-109145893-2');
-    `;
-
-    document.body.appendChild(scriptGA);
-    document.body.appendChild(script);
 }
 
-const decorateModuleRef = (modRef: any) => {
-    const appRef = modRef.injector.get(ApplicationRef);
-    const cmpRef = appRef.components[0];
-
-    const _ng = (window as any).ng;
-    enableDebugTools(cmpRef);
-    (window as any).ng.probe = _ng.probe;
-    (window as any).ng.coreTokens = _ng.coreTokens;
-    return modRef;
-};
-
 export function main(): Promise<any> {
-    const fireBugPromise = new Promise((resolve) => {
-        if (!!FIREBUG) {
-            const fb = document.createElement('script');
-            fb.type = 'text/javascript'; fb.src = '../firebug-lite/build/firebug-lite.js#startOpened,overrideConsole=false';
-            document.head.appendChild(fb);
-            const interval = setInterval(() => {
-                if (!!document.getElementById('FirebugUI')) {
-                    clearInterval(interval);
-                    resolve();
-                }
-            }, 1);
-        } else {
-            resolve();
-        }
-    });
 
-    return fireBugPromise.then(() => {
+    return fireBug.then(() => {
         platformBrowserDynamic()
             .bootstrapModule(BrowserAppModule)
             .then(decorateModuleRef)
+            .then(googleAnalytics)
             .catch((err) => console.error(err));
     });
 }
