4cf70e9264dcffee64a5c9b3c602d92466b93419
Maciej Treder <contact@maciejtreder.com>
2018-04-15 22:40:50 +0200

    updating build script

--- build_and_generate_from_schematics.sh
+++ build_and_generate_from_schematics.sh
@@ -1,8 +1,14 @@
-npm install -g @angular/cli;
+# Testing with CLI 6.x
+npm install -g @angular/cli@next;
 rm -rf application;
 cd schematics;
 npm install;
 npm run build;
 npm link;
 cd ..;
+ng new --collection angular-universal-pwa application;
+
+## testing with CLI 1.7
+rm -rf application;
+npm install -g @angular/cli;
 ng new --collection angular-universal-pwa application;
