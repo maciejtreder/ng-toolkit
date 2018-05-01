d75305675464e5dd45649e916c74d768aa32b028
Maciej Treder <contact@maciejtreder.com>
2018-05-01 20:06:30 +0200

    :hammer_and_wrench: Build fix

--- build_and_deploy_from_schematics.sh
+++ build_and_deploy_from_schematics.sh
@@ -7,7 +7,6 @@ npm link;
 cd ..;
 ng new --collection angular-universal-pwa application --gaTrackingCode UA-109145893-2;
 cd application;
-ls -la
 npm run build:deploy:aws;
 curl -I https://www.angular-universal-pwa.maciejtreder.com;
 aws sns publish --subject "New version available" --message "Check out newest version of PWA with Angular Universal by Maciej Treder! " --topic-arn arn:aws:sns:eu-central-1:$ACC_ID:sns-webpush-angular-universal
