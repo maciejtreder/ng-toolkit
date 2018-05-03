8d89a7b55f35e92f1b428a16e35b7e59a4d7eb36
Maciej Treder <contact@maciejtreder.com>
2018-05-03 19:24:26 +0200

    :hammer_and_wrench: adding necessary js files

---
+++ schematics/src/utils/new-app/files/credentials.js
@@ -0,0 +1,15 @@
+var green = "\u001b[32m";
+var white = "\u001b[22m\u001b[39m";
+var boldCyan = "\u001b[96m\u001b[1m";
+var reset = "\u001b[0m";
+
+var output =
+  green +
+  "\n\nDo you ❤️ angular-universal-pwa? \n\nStar this repo on GitHub! \n\nBecome a donor:" +
+  white +
+  "\n > " +
+  boldCyan +
+  "https://www.angular-universal-pwa.maciejtreder.com/donors\n" +
+  reset;
+
+console.log(output);
---
+++ schematics/src/utils/new-app/files/local.js
@@ -0,0 +1,7 @@
+const port = process.env.PORT || 8080;
+
+const server = require('./dist/server');
+
+server.app.listen(port, () => {
+    console.log(`Listening on: http://localhost:${port}`);
+});
