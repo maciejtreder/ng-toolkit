ee47df0fa919f6d4745d9cf4319897649ea84791
Maciej Treder <contact@maciejtreder.com>
2018-05-31 11:31:42 +0200

    :hammer_and_wrench: fixing test in init schematics

--- schematics/init/src/ng-new/index_spec.ts
+++ schematics/init/src/ng-new/index_spec.ts
@@ -26,35 +26,36 @@ describe('my-component', () => {
 
     it('should create all files of an application', () => {
         const options = { ...defaultOptions };
-        const tree = schematicRunner.runSchematic('ng-new', options, workspaceTree);
-        const files = tree.files;
+        schematicRunner.runSchematicAsync('ng-new', options, workspaceTree).subscribe(tree => {
+            const files = tree.files;
 
-        checkIfFileExists(files,'/foo/tsconfig.json');
-        checkIfFileExists(files,'/foo/tslint.json');
-        // checkIfFileExists(files,'/foo/angular.json');
-        checkIfFileExists(files,'/foo/credentials.js');
-        checkIfFileExists(files,'/foo/LICENSE');
-        checkIfFileExists(files,'/foo/local.js');
-        checkIfFileExists(files,'/foo/ngsw-config.json');
-        checkIfFileExists(files,'/foo/README.md');
-        checkIfFileExists(files,'/foo/src/environments/environment.ts');
-        checkIfFileExists(files,'/foo/src/environments/environment.prod.ts');
-        checkIfFileExists(files,'/foo/src/favicon.ico');
-        checkIfFileExists(files,'/foo/src/index.html');
-        checkIfFileExists(files,'/foo/src/main.browser.ts');
-        checkIfFileExists(files,'/foo/src/manifest.json');
-        checkIfFileExists(files,'/foo/src/main.server.ts');
-        checkIfFileExists(files,'/foo/src/polyfills.ts');
-        checkIfFileExists(files,'/foo/src/styles.css');
-        checkIfFileExists(files,'/foo/src/test.ts');
-        checkIfFileExists(files,'/foo/src/tsconfig.app.json');
-        checkIfFileExists(files,'/foo/src/tsconfig.server.json');
-        checkIfFileExists(files,'/foo/src/tsconfig.spec.json');
-        checkIfFileExists(files,'/foo/src/app/app.module.ts');
-        checkIfFileExists(files,'/foo/src/app/app.component.css');
-        checkIfFileExists(files,'/foo/src/app/app.component.html');
-        checkIfFileExists(files,'/foo/src/app/app.component.spec.ts');
-        checkIfFileExists(files,'/foo/src/app/app.component.ts');
+            checkIfFileExists(files,'/foo/tsconfig.json');
+            checkIfFileExists(files,'/foo/tslint.json');
+            // checkIfFileExists(files,'/foo/angular.json');
+            checkIfFileExists(files,'/foo/credentials.js');
+            checkIfFileExists(files,'/foo/LICENSE');
+            checkIfFileExists(files,'/foo/local.js');
+            checkIfFileExists(files,'/foo/ngsw-config.json');
+            checkIfFileExists(files,'/foo/README.md');
+            checkIfFileExists(files,'/foo/src/environments/environment.ts');
+            checkIfFileExists(files,'/foo/src/environments/environment.prod.ts');
+            checkIfFileExists(files,'/foo/src/favicon.ico');
+            checkIfFileExists(files,'/foo/src/index.html');
+            checkIfFileExists(files,'/foo/src/main.browser.ts');
+            checkIfFileExists(files,'/foo/src/manifest.json');
+            checkIfFileExists(files,'/foo/src/main.server.ts');
+            checkIfFileExists(files,'/foo/src/polyfills.ts');
+            checkIfFileExists(files,'/foo/src/styles.css');
+            checkIfFileExists(files,'/foo/src/test.ts');
+            checkIfFileExists(files,'/foo/src/tsconfig.app.json');
+            checkIfFileExists(files,'/foo/src/tsconfig.server.json');
+            checkIfFileExists(files,'/foo/src/tsconfig.spec.json');
+            checkIfFileExists(files,'/foo/src/app/app.module.ts');
+            checkIfFileExists(files,'/foo/src/app/app.component.css');
+            checkIfFileExists(files,'/foo/src/app/app.component.html');
+            checkIfFileExists(files,'/foo/src/app/app.component.spec.ts');
+            checkIfFileExists(files,'/foo/src/app/app.component.ts');
+        });
     });
 });
 
