791341abf20d1fa1287b7d51bad138b07336ef29
Maciej Treder <contact@maciejtreder.com>
2018-06-12 10:54:38 +0200

    :building_construction: adding npm registry proxy for test purposes

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,22 @@
-cd schematics
-
 CATALOGS=(_utils universal serverless init)
 
+echo "//localhost:4873/:_authToken=\"fooBar\"" >> ~/.npmrc
+
+npm install -g verdaccio
+verdaccio --config scripts/default.yaml >> verdacio_output &
+sleep 5
+head verdacio_output
+npm set registry=http://localhost:4873/
+
 exitStatus=0
+
+cd schematics
 
 for i in "${CATALOGS[@]}"
 do :
    cd $i
    npm install
-   if npm test
+   if npm publish --registry http://localhost:4873
    then
     echo $i passed
    else
@@ -17,5 +25,7 @@ do :
    cd ..
    sleep 1
 done
+
+npm set registry=https://registry.npmjs.org/
 
 exit $exitStatus
