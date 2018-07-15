4e4f815cecaaae841992a37cfd0d166872a7c2ef
Maciej Treder <contact@maciejtreder.com>
2018-07-15 13:26:10 +0200

    :hammer_and_wrench: passing firebaseProject from init to serverless (#375 fix)

--- schematics/init/src/ng-new/index.ts
+++ schematics/init/src/ng-new/index.ts
@@ -38,6 +38,7 @@ export default function(options: Schema): Rule {
 }
 
 function updatePackageJson(options: any): Rule {
+    console.log(options);
     return chain([
         (tree) => {
             addDependencyToPackageJson(tree, options, '@angular/service-worker', '^6.0.0');
--- schematics/serverless/src/index.ts
+++ schematics/serverless/src/index.ts
@@ -117,6 +117,7 @@ export default function addServerless(options: any): Rule {
 }
 `);
             let firebaseProjectSettings = {};
+            console.log(options);
             if (options.firebaseProject) {
                 firebaseProjectSettings = {
                     projects: {
