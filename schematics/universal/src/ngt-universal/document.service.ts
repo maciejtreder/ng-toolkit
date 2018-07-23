7fd6150c8be2a0439683c3a1b6023bd7d7ff2bcd
Maciej Treder <contact@maciejtreder.com>
2018-07-23 15:47:39 +0200

    :hammer_and_wrench: adding document wrapper (#378 fix)

---
+++ schematics/universal/src/ngt-universal/document.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+@Injectable()
+export class DocumentService {
+    private _document: Document;
+    constructor(@Inject(PLATFORM_ID) platformId: any) {
+        if (!isPlatformBrowser(platformId)) {
+            this._document = {
+                getElementById: (id: string) => {console.log('getElement by id'); return new HTMLElement();},
+                getElementsByClassName: (className: string) => new HTMLCollection(),
+                getElementsByName: (name: string) => new NodeList(),
+                getElementsByTagName: (name: string) => new NodeList()
+            } as Document;
+        } else {
+            console.log('in browser, assigning document');
+            // this._document = document;
+            this._document = {} as Document;
+        }
+    }
+
+    get nativeDocument(): Document {
+        console.log('get native document');
+        console.log(this._document);
+        return this._document;
+    }
+}
