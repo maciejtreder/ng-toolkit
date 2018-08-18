9c6693ed79a8bd594538be692148cc9ce209c1d3
Jeff Lu <jeff.lu@weather.com>
2018-08-18 06:27:44 -0400

    add project seo-friendly

---
+++ application/projects/seo-friendly/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
