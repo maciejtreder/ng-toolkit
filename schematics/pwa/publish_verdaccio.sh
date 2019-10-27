934eaa8b64a910e17fb807d56677c7283751073c
KingDarBoja <mbojato@uninorte.edu.co>
2019-10-27 18:39:04 -0500

    Fix deploy bash script and provide some formatting colors to help debugging

--- schematics/pwa/publish_verdaccio.sh
+++ schematics/pwa/publish_verdaccio.sh
@@ -1,8 +1,1 @@
-echo 'INSTALL INSTALL';
-npm install
-ls node_modules
-npm install
-ls node_modules
-npm test
-cd dist
 npm publish --registry http://localhost:4873
--- scripts/deploy_verdaccio.sh
+++ scripts/deploy_verdaccio.sh
@@ -1,28 +1,34 @@
-CATALOGS=(_utils pwa serverless universal firebug)
+CATALOGS=(_utils serverless pwa universal firebug)
 
-echo "//localhost:4873/:_authToken=\"CjmKyL6UDkX6FDpNnP64fw==\"" >> ~/.npmrc
+echo "//localhost:4873/:_authToken=\"CjmKyL6UDkX6FDpNnP64fw==\"" >>~/.npmrc
 
 npm install -g @angular/cli
 npm install -g verdaccio
-verdaccio --config scripts/default.yaml >> verdacio_output &
+verdaccio --config scripts/default.yaml >>verdacio_output &
 sleep 5
 head verdacio_output
-npm set registry=http://localhost:4873/
+npm set registry http://localhost:4873/
 
 exitStatus=0
 
+center() {
+  termwidth="$(tput cols)"
+  padding="$(printf '%0.1s' ={1..500})"
+  printf '\e[1;44m%*.*s %s %*.*s\e[0m\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+
 cd schematics
 
-for i in "${CATALOGS[@]}"
-do :
-    echo publishing $i
+for i in "${CATALOGS[@]}"; do
+    center "$i"
     cd $i
-    npm install
-    echo 'INSTALLED INSTALLED'
-    ls node_modules/@ng-toolkit/_utils/testing
-    if ./publish_verdaccio.sh
-    then
-        echo $i passed
+    npm install --verbose
+    printf '\e[1;32m--- NPM INSTALL COMPLETED FOR %s ---\e[0m\n' "$i"
+    if [ "$i" != "_utils" ]; then
+        ls node_modules/@ng-toolkit/_utils
+    fi
+    if ./publish_verdaccio.sh; then
+        printf 'e\[1;33m*** %s PASSED ***\e[0m\n' "$i"
     else
         exitStatus=1
     fi
